f36bc543ae52b4a5f91e1f4eca6b23a2ab343a42
Rune Flobakk <flobakk@gmail.com>
2017-01-06 21:45:22 +0100

    Use MethodType to resolve lambda result
    Replaces error-prone handcoded resolving of the lambda method signature.

--- src/main/java/co/unruly/matchers/function/DescribableFunction.java
+++ src/main/java/co/unruly/matchers/function/DescribableFunction.java
@@ -1,17 +1,18 @@
 package co.unruly.matchers.function;
 
+import java.lang.invoke.MethodType;
 import java.lang.invoke.SerializedLambda;
 import java.util.function.Function;
 
 import static co.unruly.matchers.function.DescriptionUtils.withPrefixedArticle;
-import static co.unruly.matchers.function.LambdaUtils.actualMethodOf;
 
 @FunctionalInterface
 public interface DescribableFunction<T, R> extends Function<T, R>, SerializedLambdaResolvable, SingleArgumentDescribableFunctionalInterface {
 
     default String getResultDescription() {
         SerializedLambda lambda = asSerializedLambda();
-        String resultType = actualMethodOf(lambda).getReturnType().getSimpleName();
+        MethodType lambdaMethodType = MethodType.fromMethodDescriptorString(lambda.getImplMethodSignature(), getClass().getClassLoader());
+        String resultType = lambdaMethodType.returnType().getSimpleName();
         if (! lambda.getImplMethodName().startsWith("lambda$")) {
             return lambda.getImplMethodName() + " (" + withPrefixedArticle(resultType) + ")";
         }
